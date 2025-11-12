694e91a53b41ae9078682a9c794b511d3daea79d
gr432_shefal <filippmello2007@gmail.com>
2025-11-12 14:01:02 +0700

    part 4

--- catalog/models.py
+++ catalog/models.py
@@ -33,6 +33,13 @@ class Book(models.Model):
         """
         return self.title
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
 
     def get_absolute_url(self):
         """
