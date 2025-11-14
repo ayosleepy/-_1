df35a3a55e4a371ba2ece07e76f3f845d87e083e
gr432_shefal <filippmello2007@gmail.com>
2025-11-14 10:31:27 +0700

    part 7

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 from . import views
-from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
--- catalog/views.py
+++ catalog/views.py
@@ -11,13 +11,15 @@ def index(request):
     # Доступные книги (статус = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre,},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_visits':num_visits},
     )
 from django.views import generic
 
